f2f96353ddf76c50b5f83d38da2501ef9ed0283f
HVora17 <hvora1711@gmail.com>
2023-04-22 17:29:13 -0600

    Integrated Docker and Flask Application

--- server/app.py
+++ server/app.py
@@ -1,11 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import pika
+import os
+
 
 app = Flask(__name__, template_folder='./templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # Get the data from the form
+        data = request.form['data']
+        print("DATAAAAAA: ", data)
+        command = f'curl localhost:5000/add-job/{data}'
+        # Run the curl command
+        try:
+            res = os.system(command)
+            print("RES: ", res)
+            print("EXECUTED command")
+        except:
+            print("NOT FOUNDDDDDDDDDDDDDD")
+        # Return a success message
+        
     return render_template("index.html")
 
 
@@ -17,7 +33,7 @@ def add(cmd):
     channel.basic_publish(
         exchange='',
         routing_key='task_queue',
-        body=cmd,
+        body="hiiiii",
         properties=pika.BasicProperties(
             delivery_mode=2,  # make message persistent
         ))
